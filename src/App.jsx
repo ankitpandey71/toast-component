ed97725d4c76c702a16b0556754cab7a6394dc9c
ANKIT PANDEY <116362221+ankitpandey71@users.noreply.github.com>
2024-07-31 23:37:19 +0530

    [Use] custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,62 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import useNotification from "./hooks/use-notification";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { NotificationComponent, triggerNotification } =
+    useNotification("top-right");
 
   return (
-    <>
-      <div>My new app</div>
-    </>
+    <div className="App">
+      {NotificationComponent}
+      <h1>Toast Component</h1>
+      <div className="btns">
+        <button
+          onClick={() =>
+            triggerNotification({
+              type: "success",
+              message: "This is a success message!",
+              duration: 3000,
+              animation: "pop",
+            })
+          }
+        >
+          Show Success
+        </button>
+        <button
+          onClick={() =>
+            triggerNotification({
+              type: "info",
+              message: "This is an info message!",
+              duration: 3000,
+            })
+          }
+        >
+          Show Info
+        </button>
+        <button
+          onClick={() =>
+            triggerNotification({
+              type: "warning",
+              message: "This is a warning message!",
+              duration: 3000,
+            })
+          }
+        >
+          Show Warning
+        </button>
+        <button
+          onClick={() =>
+            triggerNotification({
+              type: "error",
+              message: "This is an error message!",
+              duration: 3000,
+            })
+          }
+        >
+          Show Error
+        </button>
+      </div>
+    </div>
   );
 }
 
